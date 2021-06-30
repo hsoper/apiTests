6596aa564fca48cb467b90aa4b2cf48a6ea6e671
Humberto Soperanez Moya <hsoperanezmoya@hawk.iit.edu>
2021-06-30 00:45:52 +0000

    Fixed Style

--- astro.py
+++ astro.py
@@ -1,9 +1,9 @@
-import requests 
+import requests
 
 response = requests.get("http://api.open-notify.org/astros.json")
 data = response.json()
-print("Status Code :",response.status_code)
+print("Status Code :", response.status_code)
 people = data['people']
 
 for i in range(0,5):
-  print(people[i]['name'])
+     print(people[i]['name'])
