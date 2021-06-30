9e16775bd66ad76bd91c2e421a5122e47eebc914
Humberto Soperanez Moya <hsoperanezmoya@hawk.iit.edu>
2021-06-30 21:08:07 +0000

    Refactor

--- crypto.py
+++ crypto.py
@@ -1,31 +1,54 @@
 import requests
 import json
-from pycoingecko import CoinGeckoAPI
 import pandas as pd
 from sqlalchemy import create_engine
 
-response = requests.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-data = response.json()['bpi']['USD']['rate']
-h = CoinGeckoAPI()
-data = h.get_price(ids='dogecoin', vs_currencies='usd')['dogecoin']['usd']
-coins = h.get_price(ids='ethereum,monero,tether', vs_currencies='usd')
-print(coins)
-coin = []
-priceUSD = []
-for key, usd in coins.items():
-    coin.append(key)
-    for k, i in usd.items():
-        priceUSD.append(float(i))
-user = input("What cryptocurrency's price do you want to know?\n")
-user = user.lower()
-coins = h.get_price(ids=user, vs_currencies='usd')
-for key, item in coins.items():
-    if key == user:
-        coin.append(key)
-        priceUSD.append(float(item['usd']))
-print(coin, f"\n{priceUSD}")
-dcoins = {"Coin": coin, "PriceUSD": priceUSD}
-dcoins = pd.DataFrame.from_dict(dcoins)
+baseURL = 'https://api.coingecko.com/api/v3/simple/price?ids='
+
+def get_coingecko_json(coin):
+  response = requests.get(baseURL+coin+'&vs_currencies=usd')
+  print(response.status_code)
+  return response.json()
+
+
+def get_names_and_usd(json):
+    temp = [[],[]]
+    for key, usd in json.items():
+        temp[0].append(key)
+        for k, i in usd.items():
+            temp[1].append(float(i))
+    return temp
+
+  
+def get_users_coinusd():
+    user = input("What cryptocurrency's price do you want to know?\n")
+    user.lower()
+    x=0
+    js = get_coingecko_json(user)
+    return js
+
+  
+def append_json_values(lis,json):
+    temp = get_names_and_usd(json)
+    for i in range(0,len(temp)):
+        for x in temp[i]:
+            lis[i].append(x)
+    return lis
+
+  
+def make_dataframe(lis):
+    dcoins = {"Coin": coin_usd[0], "PriceUSD": coin_usd[1]}
+    dcoins = pd.DataFrame.from_dict(dcoins)
+    return dcoins
+
+def sendto_database(datafr,database,table):
+    engine = create_engine('mysql://root:codio@localhost/'+database)
+    datafr.to_sql(table, con=engine, if_exists='replace', index=False)
+    
+    
+coins = get_coingecko_json('ethereum,monero,tether')
+coin_usd = get_names_and_usd(coins)
+coin_usd = append_json_values(coin_usd,get_users_coinusd())
+dcoins = make_dataframe(coin_usd)
 print(dcoins)
-engine = create_engine('mysql://root:codio@localhost/Crypto')
-dcoins.to_sql('CoinPrices', con=engine, if_exists='replace', index=False)
+sendto_database(dcoins,engine,'CoinPrices')
