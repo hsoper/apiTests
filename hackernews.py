2c9d815b827fa8f1f1318ca044e7dd110423952a
Humberto Soperanez Moya <hsoperanezmoya@hawk.iit.edu>
2021-06-30 00:57:05 +0000

    Fixed Style

--- hackernews.py
+++ hackernews.py
@@ -7,14 +7,14 @@ url = f'https://hacker-news.firebaseio.com/v0/item/{code}.json?print=pretty'
 response = requests.get(url)
 story = response.json()
 while story['type'] != 'story':
-  code -= 1
-  url = f'https://hacker-news.firebaseio.com/v0/item/{code}.json?print=pretty'
-  response = requests.get(url)
-  story = response.json()
+    code -= 1
+    url = f'https://hacker-news.firebaseio.com/v0/item/{code}.json?print=pretty'
+    response = requests.get(url)
+    story = response.json()
 url = "No link was given"
 by = "No author was given"
 title = "No title was given"
-for key,item in story.items():
+for key, item in story.items():
   if key == 'title':
     title = item
   elif key == 'by':
--- test.py
+++ test.py
@@ -7,7 +7,8 @@ print('This is the current bitcoin price according to coindesk :',data)
 h = CoinGeckoAPI()
 data = h.get_price(ids='dogecoin', vs_currencies='usd')['dogecoin']['usd']
 print("This is the current dogecoin price according to CoinGecko : ",data)
-print('These are the current ethereum, monero, and tether prices according to CoinGecko')
+print('These are the current ethereum' +
+      'monero, and tether prices according to CoinGecko')
 coins = h.get_price(ids='ethereum,monero,tether', vs_currencies='usd')
 monero = coins['monero']['usd']
 ethereum = coins['ethereum']['usd']
