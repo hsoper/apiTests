b39d17d2a90d041ef28ec14e32ad38fa9d9525cb
Humberto Soperanez Moya <hsoperanezmoya@hawk.iit.edu>
2021-06-30 00:43:01 +0000

    Fixed Style

--- hackernews.py
+++ hackernews.py
@@ -1,17 +1,16 @@
 import requests
 url = 'https://hacker-news.firebaseio.com/v0/maxitem.json?print=pretty'
 response = requests.get(url)
+data = response.json()
 code = int(data)
 url = f'https://hacker-news.firebaseio.com/v0/item/{code}.json?print=pretty'
 response = requests.get(url)
 story = response.json()
-
 while story['type'] != 'story':
   code -= 1
   url = f'https://hacker-news.firebaseio.com/v0/item/{code}.json?print=pretty'
   response = requests.get(url)
   story = response.json()
-print(story)
 url = "No link was given"
 by = "No author was given"
 title = "No title was given"
