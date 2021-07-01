616ff839144d035b5eb28571a17fd742440d1825
Humberto Soperanez Moya <hsoperanezmoya@hawk.iit.edu>
2021-07-01 01:45:55 +0000

    Unit Testing

--- crypto.py
+++ crypto.py
@@ -3,52 +3,65 @@ import json
 import pandas as pd
 from sqlalchemy import create_engine
 
-baseURL = 'https://api.coingecko.com/api/v3/simple/price?ids='
-
+# Test when if the input is a supported crypto in the correct format
+# Check for null inputs
 def get_coingecko_json(coin):
+  baseURL = 'https://api.coingecko.com/api/v3/simple/price?ids='
   response = requests.get(baseURL+coin+'&vs_currencies=usd')
-  print(response.status_code)
   return response.json()
 
 
+# check if the function returns a 2d array and not a none object
 def get_names_and_usd(json):
-    temp = [[],[]]
+    temp = [[], []]
     for key, usd in json.items():
         temp[0].append(key)
         for k, i in usd.items():
             temp[1].append(float(i))
     return temp
 
-  
+
+# check if the user inputs a correct file type
 def get_users_coinusd():
     user = input("What cryptocurrency's price do you want to know?\n")
     user.lower()
-    x=0
     js = get_coingecko_json(user)
     return js
 
-  
+
+# check when the input is a null for the list
+# check when the json file is empty
 def append_json_values(lis,json):
+    
     temp = get_names_and_usd(json)
+    if type(lis) is not list:
+        return temp
+        
     for i in range(0, len(temp)):
         for x in temp[i]:
             lis[i].append(x)
     return lis
 
-  
-def make_dataframe(lis):
+ 
+# check when the input is an empty list/invalid
+# check when when the list contains the wrong types. (not a string or float)
+def make_dataframe(coin_usd):
     dcoins = {"Coin": coin_usd[0], "PriceUSD": coin_usd[1]}
     dcoins = pd.DataFrame.from_dict(dcoins)
     return dcoins
 
-def sendto_database(datafr,database,table):
-    engine = create_engine('mysql://root:codio@localhost/'+database)
+
+# no return value
+# Possibly check the inputs of the method
+def sendto_database(datafr, database, table):
+    engine = create_engine('mysql://root:codio@localhost/'+ database)
     datafr.to_sql(table, con=engine, if_exists='replace', index=False)
+
     
-    
-coins = get_coingecko_json('ethereum,monero,tether')
-coin_usd = get_names_and_usd(coins)
-coin_usd = append_json_values(coin_usd, get_users_coinusd())
-dcoins = make_dataframe(coin_usd)
-print(dcoins)
-sendto_database(dcoins, "crypto", 'CoinPrices')
+
+# coins = get_coingecko_json('ethereum,monero,tether')
+# coin_usd = get_names_and_usd(coins)
+# coin_usd = append_json_values(coin_usd, get_users_coinusd())
+# dcoins = make_dataframe(coin_usd)
+# print(dcoins)
+# sendto_database(dcoins, "crypto", 'CoinPrices')
---
+++ testcrypto.py
@@ -0,0 +1,58 @@
+import unittest
+import requests
+import pandas as pd
+from crypto import get_coingecko_json, get_names_and_usd
+from crypto import get_users_coinusd, append_json_values
+from crypto import make_dataframe, sendto_database
+
+
+class TestFileName(unittest.TestCase):
+    # The function should not return an empty string. Might add some default crypto
+    # the users as input if the user inputs an invalid 
+    def test_get_coingecko_json(self):
+        baseURL = 'https://api.coingecko.com/api/v3/simple/price?ids='
+        response = requests.get(baseURL+"coin"+'&vs_currencies=usd')
+        self.assertNotEqual(get_coingecko_json("invalid"), response.json() )
+        self.assertNotEqual(get_coingecko_json(2424), response.json() )
+        self.assertNotEqual(get_coingecko_json(None), None )
+        
+        
+    # The function should not return an empty list.
+    def test_get_names_and_usd(self):
+        baseURL = 'https://api.coingecko.com/api/v3/simple/price?ids='
+        response = requests.get(baseURL+"coin"+'&vs_currencies=usd')
+        self.assertNotEqual(len(get_names_and_usd(response.json())[0]),0)
+        self.assertNotEqual(len(get_names_and_usd(response.json())[1]),0)
+
+
+    # The function should not return an empty json file.
+    # Reprompt the user or deal with the problem in 
+    # get_coingecko_json(st) since this function calls it.
+    def test_get_users_coinusd(self):
+        baseURL = 'https://api.coingecko.com/api/v3/simple/price?ids='
+        response = requests.get(baseURL+"coin"+'&vs_currencies=usd')
+        self.assertNotEqual(get_users_coinusd(),response.json())
+
+
+    #method should always return whatever the json file is going to append
+    #even if the user doesn't input a correct list format
+    def test_append_json_values(self):
+        baseURL = 'https://api.coingecko.com/api/v3/simple/price?ids='
+        response = requests.get(baseURL+"bitcoin"+'&vs_currencies=usd')
+        temp = get_names_and_usd(response.json())
+        self.assertEqual(append_json_values([[],[]],response.json()),temp)
+        self.assertEqual(append_json_values("list",response.json()),temp)
+        self.assertEqual(append_json_values(242,response.json()),temp)
+    
+    
+    #Should return an empty dataframe if invalid inputs
+    #tells the user
+    def test_make_data_frame(self):
+        l = [[]]
+        self.assertEqual(make_dataframe(l),pd.DataFrame.from_dict({}))
+        self.assertEqual(make_dataframe(1),pd.DataFrame.from_dict({}))
+        self.assertEqual(make_dataframe("hello"),pd.DataFrame.from_dict({}))
+        
+
+if __name__ == '__main__':
+    unittest.main()
