9a5b4cac35e92dceb0f402704d52e350a6f26f57
Humberto Soperanez Moya <hsoperanezmoya@hawk.iit.edu>
2021-07-02 16:03:52 +0000

    New Folder

--- Tests/astro.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get("http://api.open-notify.org/astros.json")
-data = response.json()
-print("Status Code :", response.status_code)
-people = data['people']
-
-for i in range(0, 5):
-    print(people[i]['name'])
